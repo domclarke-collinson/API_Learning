1ef31dfbb040d295838ebfc136c6fa461e448e94
Dom Clarke <dom.clarke@collinsongroup.com>
2025-09-16 21:05:56 +0100

    "feat/addingtablestodb"

--- database/init.sql
+++ database/init.sql
@@ -1,13 +1,22 @@
-CREATE TABLE IF NOT EXISTS memberships (
+Begin;
+
+CREATE TABLE IF NOT EXISTS memberships ( --create table if it does not exist
   id SERIAL PRIMARY KEY, -- Counts from 0 for membership unique ID
   name VARCHAR(100) NOT NULL, -- Name of membership
   email VARCHAR(255) UNIQUE NOT NULL, -- Makes the emails unique
   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() -- Date of creation
 );
 
-CREATE TABLE deals ( 
-  id SERIAL PRIMARY KEY,
-  membership_id INT REFERENCES memberships(id),
-  description TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS deals ( --create table if it does not exist
+  id SERIAL PRIMARY KEY, -- Counts from 0 for membership unique ID
+  membership_id INTEGER NOT NULL --checks for integer
+    REFERENCES memberships(id) --references back to the membership table ID's
+    ON DELETE CASCADE, --autodelete if membershio is deleted
+  description TEXT, -- desciption in text format
+  amount NUMERIC(10,2) DEFAULT 0, --decimals for money
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() -- Date of creation
 );
+
+CREATE INDEX IF NOT EXISTS idx_deals_membership_id ON deals(membership_id); --makes an index to search fater
+
+COMMIT; 
