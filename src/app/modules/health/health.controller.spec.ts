1c90b53e4ad82942740145ba78fc03a4198e6d55
Dom Clarke <dom.clarke@collinsongroup.com>
2026-01-21 20:57:21 +0000

    feat/adding github checks

---
+++ src/app/modules/health/health.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthController } from './health.controller';
+
+describe('HealthController Unit', () => {
+  let controller: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+    }).compile();
+
+    controller = module.get<HealthController>(HealthController);
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+
+  it("returns 200 when Health check is successful", () => {
+    const result = controller.healthCheck();
+    expect(result).toMatchObject({ status: 'ok' });
+  });
+});
